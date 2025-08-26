5b83da039bfcb2b4ce733dc350498edf446bd6bc
Antonio C Cadamuro <cadamuro@tm2b.com.br>
2025-08-25 21:35:34 -0300

    up contact views urls 20250825

--- TMCore/migrations/0001_initial.py
+++ TMCore/migrations/0001_initial.py
@@ -33,7 +33,7 @@ class Migration(migrations.Migration):
             },
         ),
         migrations.CreateModel(
-            name='Currency',
+            name='tm_currency',
             fields=[
                 ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('current_status', models.BooleanField(default=True)),
